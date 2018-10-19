3058f9d6b03ca38e7a8c4b88ea8de1385a2463fb
LuisFernandoBH <lfrs@outlook.com>
2018-10-19 20:48:53 -0300

    teste angular 1

--- ECN/SiteAssets/ECN/js/spDataServices.js
+++ ECN/SiteAssets/ECN/js/spDataServices.js
@@ -1,2 +1,26 @@
 //URL do Site
 var siteUrl = 'https://spg-portal.schenckprocess.com/sites/LM';
+
+
+var appVar = angular.module('listApp', []);  
+appVar.controller("controller1", function($scope){    
+    GetListItems($scope, "teste");    
+});    
+appVar.controller("controller2", function($scope){    
+    GetListItems($scope, "teste");    
+});   
+
+function GetListItems($scope, listName){    
+    $.ajax({    
+        url: "/_api/web/lists/GetByTitle('"+listName+"')/items",    
+        method: "GET",    
+        async: false,    
+        headers: { "Accept": "application/json;odata=verbose" },    
+        success: function(data){    
+            $scope.items = data.d.results;    
+        },    
+        error: function(sender,args){    
+            console.log(args.get_message());    
+        }    
+    });    
+}    
